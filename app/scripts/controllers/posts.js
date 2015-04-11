ef0b7f1da01e7d38c3fb9e4759be5519420506f3
Carlos Victorica <carlos@monsoonco.com>
2015-04-10 19:01:44 -0700

    implement save method

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -6,9 +6,10 @@ app.controller('PostsCtrl', function($scope, Post) {
   $scope.post = {url: 'http://', title: ''};
 
   $scope.submitPost = function() {
-    Post.save($scope.post);
-    // $scope.posts.push($scope.post);
-    $scope.post = {url: 'http://', title: ''};
+    Post.save($scope.post, function (ref) {
+      $scope.posts[ref.name] = $scope.post;
+      $scope.post = {url: 'http://', title: ''};
+    });
   };
 
   $scope.deletePost = function(index) {
