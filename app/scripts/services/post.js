4135b1e046282e9f07e86f918bbd3ed7d069b7bf
Carlos Victorica <carlos@monsoonco.com>
2015-04-11 15:31:31 -0700

    controller talks to firebase instance only

--- app/scripts/services/post.js
+++ app/scripts/services/post.js
@@ -1,6 +1,22 @@
 'use strict';
 
-app.factory('Post', function($firebase, FIREBASE_URL) {
-  var ref = new Firebase(FIREBASE_URL);
-  var posts = $firebase(ref.child('posts')).$asArray();
+app.factory('Post', function($firebaseArray, FIREBASE_URL) {
+  //var ref = new Firebase(FIREBASE_URL);
+  var postsRef = new Firebase(FIREBASE_URL + 'posts');
+  var posts = $firebaseArray(postsRef);
+
+  var Post = {
+    all: posts,
+    create: function (post) {
+      return posts.$add(post);
+    },
+    get: function (postId) {
+      return posts.$getRecord(postId);
+    },
+    delete: function(postId) {
+      return posts.$remove(postId);
+    }
+  }
+
+  return Post;
 });
